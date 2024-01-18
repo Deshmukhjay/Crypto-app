30b054f2c67505e1289838308181fb0e6940d32f
deshmukhjay <deshmukhjay2002@gmail.com>
2024-01-18 23:28:02 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React,{StrictMode} from 'react';
+import { ChakraProvider } from '@chakra-ui/react'
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <ChakraProvider>
+    <App />
+    </ChakraProvider>
+    </StrictMode>
+);
+
+export const server=`https://api.coingecko.com/api/v3`;
